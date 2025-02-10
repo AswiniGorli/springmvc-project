958fa0db3d04d465d3ca1e04921d594aad4a6030
AswiniGorli <aswinigorli17@gmail.com>
2025-02-10 23:26:55 +0530

    updated

--- src/main/java/com/example/demo/services/impli/serviceimpli.java
+++ src/main/java/com/example/demo/services/impli/serviceimpli.java
@@ -4,12 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.models.springmvc;
 import com.example.demo.services.springmvcservice;
-@Service
-public class serviceimpli implements springmvcservice{
 
-	@Override
-	public void adduser(springmvc s)
-	{
-		
-	}
+@Service
+public class serviceimpli{
+
+	
 }
